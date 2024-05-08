5f170573108c350f253132e16cfe71378591ff19
winidi <dirkwiniecki@web.de>
2024-05-08 06:45:20 +0200

    template

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <div class="hero min-h-screen bg-base-200">
       <div class="hero-content text-center">
         <div class="max-w-md">
-          <h1 class="text-5xl font-bold">Welcome to CommentTheWeb Through X</h1>
+          <h1 class="text-5xl font-bold">Welcome</h1>
           <p class="py-6">
-           Comment The web is a platform that allows you to comment on any website you want through X. 
+          Bam
           </p>
           <button class="btn btn-primary">Get Started (Soon)</button>
         </div>
